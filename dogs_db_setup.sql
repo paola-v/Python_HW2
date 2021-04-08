771e519bc56461d9915b7935c9e9ea0f0e7c5a87
Paola Ignatova <paola.ignatova@gmail.com>
2021-04-08 18:29:22 -0500

    sql clean up code

--- dogs_db_setup.sql
+++ dogs_db_setup.sql
@@ -1,5 +1,13 @@
 USE dogs_db;
-CREATE TABLE dogs(
+
+
+# Drop tables if they exist
+DROP TABLE IF EXISTS DOGS;
+DROP TABLE IF EXISTS OWNERS;
+
+
+# Create tables to match db models in main.py
+CREATE TABLE IF NOT EXISTS dogs(
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100),
 breed VARCHAR(100),
@@ -9,14 +17,17 @@ mood ENUM('playful','excited','relaxed','fearful'),
 sex ENUM('girl','boy')
 );
 
-CREATE TABLE owners(
+CREATE TABLE IF NOT EXISTS owners(
 id INTEGER PRIMARY KEY AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100),
 phone VARCHAR(100),
 email VARCHAR(100),
-dog_id INT
+dog_id INT,
+FOREIGN KEY (dog_id) REFERENCES dogs(id)
 );
 
+
+# Check to make sure the tables were created 
 SELECT * FROM dogs;
 
 SELECT * FROM owners;
